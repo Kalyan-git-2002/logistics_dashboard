de80b8917ee586c6f4f0662b06c24b8df6a02f58
Kalyan-git-2002 <addepallikalyan88@gmail.com>
2026-03-26 10:44:55 +0530

    Switch from Polars to Pandas for data export
    Refactor data export to use pandas and S3 filesystem.

--- export_data.py
+++ export_data.py
@@ -1,30 +1,19 @@
-import polars as pl
-import os
+import pandas as pd
+import s3fs,os
 
-storage_options = {
-    "AWS_ACCESS_KEY_ID"     : os.environ["AWS_ACCESS_KEY"],
-    "AWS_SECRET_ACCESS_KEY" : os.environ["AWS_SECRET_KEY"],
-    "AWS_REGION"            : "us-east-1"
-}
-
-BUCKET = "s3://clg-demo-2026"
-
-# Read Gold Delta tables
-print("Reading fact_deliveries...")
-fact_df = pl.read_parquet(
-    f"{BUCKET}/powerbi/fact",
-    storage_options = storage_options
-)
-
-print("Reading dim_date...")
-dim_df = pl.read_parquet(
-    f"{BUCKET}/powerbi/dim/dim_date/",
-    storage_options = storage_options
-)
-
-# Save CSV
-fact_df.write_csv("fact_deliveries.csv")
-dim_df.write_csv("dim_date.csv")
-
-print(f"✅ fact → {len(fact_df)} rows")
-print(f"✅ dim  → {len(dim_df)} rows")
+try:
+    fs = s3fs.S3FileSystem(
+        key=os.getenv("AWS_ACCESS_KEY"),
+        secret=os.getenv("AWS_SECRET_KEY"))
+    
+    bucket_1 = "clg-demo-2026/powerbi/fact/"
+    bucket_2 = "clg-demo-2026/powerbi/dim/dim_date/"
+    df_1 = pd.read_parquet(bucket_1,filesystem=fs,filters=None)
+    df_2 = pd.read_parquet(bucket_2,filesystem=fs,filters=None)
+    
+    df_1.to_csv("fact.csv",index=False)
+    df_2.to_csv("dim_date.csv",index=False)
+except Exception as  e:
+    print(f"ERROR : {e}")
+    exit 1
+    
