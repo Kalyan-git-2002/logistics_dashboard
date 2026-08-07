3972077a39225657df6a447ec6c97a52fbd7e0ee
Kalyan-git-2002 <addepallikalyan88@gmail.com>
2026-08-08 00:14:04 +0530

    Fix typo in dtype_backend parameter

--- export_data.py
+++ export_data.py
@@ -8,8 +8,8 @@ try:
     
     bucket_1 = "clg-demo-2026/powerbi/fact/"
     bucket_2 = "clg-demo-2026/powerbi/dim/dim_date/"
-    df_1 = pd.read_parquet(bucket_1,filesystem=fs,filters=None,dtype_backend ='nummpy_nullable')
-    df_2 = pd.read_parquet(bucket_2,filesystem=fs,filters=None,dtype_backend ='nummpy_nullable')
+    df_1 = pd.read_parquet(bucket_1,filesystem=fs,filters=None,dtype_backend ='numpy_nullable')
+    df_2 = pd.read_parquet(bucket_2,filesystem=fs,filters=None,dtype_backend ='numpy_nullable')
     
     df_1.to_csv("fact.csv",index=False)
     df_2.to_csv("dim_date.csv",index=False)
