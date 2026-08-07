4d5e667f0adf6ab3a1564c2c747cbbaee4c6bc50
Kalyan-git-2002 <addepallikalyan88@gmail.com>
2026-08-08 01:02:04 +0530

    Refactor export_data.py to use DeltaTable for S3
    Updated the export_data.py script to use DeltaTable for reading data from S3 instead of pandas directly. Adjusted storage options and bucket paths accordingly.

--- export_data.py
+++ export_data.py
@@ -1,15 +1,20 @@
 import pandas as pd
-import s3fs,os
+import os
+from deltalake import DeltaTable
 
 try:
-    fs = s3fs.S3FileSystem(
-        key=os.getenv("AWS_ACCESS_KEY"),
-        secret=os.getenv("AWS_SECRET_KEY"))
+    storage_options = {
+    "AWS_ACCESS_KEY_ID":os.getenv("AWS_ACCESS_KEY"),
+    "AWS_SECRECT_ACCESS_KEY:"os.getenv("AWS_SECRET_KEY")}
     
-    bucket_1 = "clg-demo-2026/powerbi/fact/"
-    bucket_2 = "clg-demo-2026/powerbi/dim/dim_date/"
-    df_1 = pd.read_parquet(bucket_1,filesystem=fs,filters=None)
-    df_2 = pd.read_parquet(bucket_2,filesystem=fs,filters=None)
+    bucket_1 = "s3://clg-demo-2026/gold/fact/"
+    bucket_2 = "s3://clg-demo-2026/gold/dim/dim_date/"
+
+    dt_1 = DeltaTable(bucket_1,storage_options = storage_options)
+    dt_2 = DeltaTable(bucket_2,storage_options = storage_options)
+    
+    df_1 = dt_1.to_pandas()
+    df_2 = dt_2.to_pandas()
     
     df_1.to_csv("fact.csv",index=False)
     df_2.to_csv("dim_date.csv",index=False)
