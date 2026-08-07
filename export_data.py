e6100aac39834b1d7ad08b1af132df9372bdb4e0
Kalyan-git-2002 <addepallikalyan88@gmail.com>
2026-08-08 00:28:59 +0530

    Update export_data.py

--- export_data.py
+++ export_data.py
@@ -8,8 +8,8 @@ try:
     
     bucket_1 = "clg-demo-2026/powerbi/fact/"
     bucket_2 = "clg-demo-2026/powerbi/dim/dim_date/"
-    df_1 = pd.read_parquet(bucket_1,filesystem=fs,filters=None,dtype_backend ='pyarrow')
-    df_2 = pd.read_parquet(bucket_2,filesystem=fs,filters=None,dtype_backend ='pyarrow')
+    df_1 = pd.read_parquet(bucket_1,filesystem=fs,filters=None)
+    df_2 = pd.read_parquet(bucket_2,filesystem=fs,filters=None)
     
     df_1.to_csv("fact.csv",index=False)
     df_2.to_csv("dim_date.csv",index=False)
